ba7dc694906407616f8760c5382a40c4e383c6f6
Mohamed Hasan M <mohamedhasan8403@gmail.com>
2026-02-15 15:52:15 +0700

    Update APIHelper.js

--- backend/helper/APIHelper.js
+++ backend/helper/APIHelper.js
@@ -2,7 +2,7 @@ class APIHelper
 {
     constructor(query, queryStr) {
         this.query =query // MongoDB Query
-        this.queryStr = queryStr // Query String From URL
+        this.queryStr = queryStr // Query String From URL 
     }
     search() {
         const keyword = this.queryStr.keyword? {
